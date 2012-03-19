1844ee2a74681f44e42cc78534a6d349fcf6cd2c
Samuel Kay <samuel-kay@401kay.fr>
2012-03-19 23:14:45 +0100

    Substitition des fonctions en interruption par des marco multilignes

--- BrasRobot/servo/servo.h
+++ BrasRobot/servo/servo.h
@@ -20,5 +20,47 @@
 /** P R I V A T E  P R O T O T Y P E S ***************************************/
 void Servo_Set(unsigned int,char);
 void Servo_Init(void);
-void Servo_Int(void);
 int Servo_Get(char);
+
+/** V A R I A B L E S ********************************************************/
+extern volatile char timer_servo; // Compteur qui permet d'avoir une periode de 10 ms.
+extern volatile char servo_courant; // Compteur qui sauvegarde le servo courant.
+extern volatile int pos_servo[NB_SERVO]; 		// Tableau qui contient les valeurs 
+
+
+#define Servo_Int() {                                      \
+	if (PIR1bits.TMR2IF)                                   \
+	{                                                      \
+		PIR1bits.TMR2IF = 0;                               \
+		WriteTimer2(0);                                    \
+		timer_servo--;                                     \
+		if(timer_servo <= 0){                              \
+			if(servo_courant == 0){                        \
+				SERVO1=1;                                  \
+				timer_servo = pos_servo[0] >> 8;           \
+				if((pos_servo[servo_courant] & 0x00FF) != 0){ \
+					WriteTimer2((unsigned char)(0x100 - (pos_servo[servo_courant] & 0x00FF))); \
+				}else{                                     \
+					WriteTimer2(0);                        \
+					timer_servo--;                         \
+				}                                          \
+				servo_courant = 1;                         \
+			}else if(servo_courant == 1){                  \
+    			SERVO1=0;                                  \
+				SERVO2=1;                                  \
+				timer_servo = pos_servo[1] >> 8;           \
+				if((pos_servo[servo_courant] & 0x00FF) != 0){  \
+					WriteTimer2((unsigned char)(0x100 - (pos_servo[servo_courant] & 0x00FF))); \
+				}else{                                     \
+					WriteTimer2(0);                        \
+					timer_servo--;                         \
+				}                                          \
+				servo_courant = 2;                         \
+			}else{                                         \
+				servo_courant = 0;                         \
+				SERVO2=0;                                  \
+				timer_servo = 25;                          \
+			}                                              \
+		}		                                           \
+	}                                                      \
+}
