ff80164299620e10c8cab22722b1b6a5a50ff426
Samuel Kay <samuel-kay@401kay.fr>
2012-05-01 17:20:33 +0200

    Attrappe Lingot : Modification de la position des doigts

--- BrasRobot/include/servo.h
+++ BrasRobot/include/servo.h
@@ -8,12 +8,14 @@
 
 // SERVO 1 
 #define DOIGT_G_OUVERT 0x2D0,0
-#define DOIGT_G_TIRE   0x4D0,0
+//4D0
+#define DOIGT_G_TIRE   0x460,0
 #define DOIGT_G_RABAT  0x5D0,0
 #define DOIGT_G_FERME  0x710,0
 // SERVO 2 (marqué servo 1 sur la carte réelle)
 #define DOIGT_D_OUVERT 0x710,1
-#define DOIGT_D_TIRE   0x590,1
+//590
+#define DOIGT_D_TIRE   0x5F0,1
 #define DOIGT_D_RABAT  0x410,1
 #define DOIGT_D_FERME  0x2D0,1
 
